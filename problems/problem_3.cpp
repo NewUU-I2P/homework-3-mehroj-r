ec86906a865aa0b1749f90dcfcbd3b9efb17c1c6
MEHROJ <mekhrojmajidov@gmail.com>
2023-11-08 05:30:42 +0500

    Solved Problem 3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,27 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
+    std::string heightReport;
 
+    if (S == 'M'){
+        if (height < 1.7){
+            heightReport = "Short";
+        } else if (height >= 1.7 && height < 1.85){
+            heightReport = "Normal";
+        } else if (height >= 1.85){
+            heightReport = "Tall";
+        }
+    } else if (S == 'F'){
+        if (height < 1.6){
+            heightReport = "Short";
+        } else if (height >= 1.6 && height < 1.75){
+            heightReport = "Normal";
+        } else if (height >= 1.75){
+            heightReport = "Tall";
+        }
+    }
+
+    return heightReport;
     // use return to return your result
     // make use of control flow statements
 }
